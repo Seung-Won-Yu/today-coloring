f2f1d57bc3b25fbe53b123423928c4314c667c6d
Seung-Won-Yu <Seung-Won-Yu@users.noreply.github.com>
2026-06-15 17:54:18 +0900

    chore: trim active artworks by fill complexity

--- js/data/artworks.js
+++ js/data/artworks.js
@@ -10,19 +10,17 @@ var THEME_HINTS = {
 var ARTWORK_CATALOG = [
   ['squirrel', '다람쥐', '동물', 'squirrel.png'],
   ['whale', '고래', '동물', 'whale.png'],
+  ['cat', '고양이', '동물', 'cat.png'],
+  ['deer', '사슴', '동물', 'deer.png'],
+  ['hippo', '하마', '동물', 'hippo.png'],
+  ['vase', '화병', '꽃과 그릇', 'vase.png'],
+  ['lotus', '연꽃', '꽃과 그릇', 'lotus.png'],
   ['sunflower', '해바라기', '꽃과 그릇', 'sunflower.png'],
   ['jars', '항아리', '전통 소품', 'jars.png'],
+  ['watermill', '물레방아', '자연 풍경', 'watermill.png'],
   ['fan', '부채', '전통 소품', 'fan.png'],
-  ['fruit_basket', '과일 바구니', '먹거리', 'fruit-basket.png'],
-  ['dragon_mask', '용탈', '전통 소품', 'dragon-mask.png'],
-  ['crane', '두루미', '동물', 'crane.png'],
-  ['janggu', '장구', '전통 소품', 'janggu.png'],
-  ['butterfly', '나비', '동물', 'butterfly.png'],
-  ['turtle', '바다거북', '동물', 'turtle.png'],
-  ['peacock', '공작새', '동물', 'peacock.png'],
-  ['owl', '부엉이', '동물', 'owl.png'],
-  ['goldfish', '금붕어', '동물', 'goldfish.png'],
-  ['mandala_flower', '꽃 만다라', '꽃과 그릇', 'mandala-flower.png']
+  ['mask', '탈', '전통 소품', 'mask.png'],
+  ['janggu', '장구', '전통 소품', 'janggu.png']
 ];
 
 window.ARTWORKS = ARTWORK_CATALOG.map(function(entry) {
